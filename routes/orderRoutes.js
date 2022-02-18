aeb64d69a83dceeb18233c8d70302b3801ef1d54
deelesisuanu <deelesisuanu@gmail.com>
2022-02-18 04:03:49 +0100

    updates

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,6 +10,6 @@ const router = express.Router();
 
 router.route("/users/orders/:customer_id").post(createOrder).get(myOrdersAll);
 router.route("/users/orders/:order_id").put(updateOrderPaid);
-router.route("/users/orders/i/:order_id").get(orderById);
+router.route("/users/orders/i/:order_id").get(orderById); 
 
 module.exports = router;
